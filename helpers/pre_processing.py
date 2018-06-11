817a7d1b393d19c6e14c80842d6abbd26462bf0e
Bobby <bobby.w.lindsey@gmail.com>
2018-06-11 17:51:02 -0500

    implemented ngram idf

--- helpers/pre_processing.py
+++ helpers/pre_processing.py
@@ -4,6 +4,9 @@ import math
 from collections import defaultdict
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 # Pipeline friendly functions
@@ -36,13 +39,13 @@ def strip_whitespace():
 
 
 def string_to_float():
-    def roboust_string_to_float(x):
+    def robust_string_to_float(x):
         try:
             if type(x) == str: return float(x)
             else: return x
         except:
             return x
-    return roboust_string_to_float
+    return robust_string_to_float
 
 
 def remove_string(string_to_remove):
@@ -203,6 +206,56 @@ def parse_date(df, date_column_name):
     return df
 
 
+def ngram_tf(n, min_doc_freq, max_doc_freq, variables, print_stats=False):
+    """
+    Get ngram dictionary from pandas series
+    :param n: int
+    :param min_doc_freq: float
+    :param max_doc_freq: float
+    :param variables: pandas.Series or list of pandas.Series
+    :param print_stats: boolean
+    :return: pandas.DataFrame
+    """
+    # stack the variables into one if a list is passed
+    if type(variables) == list:
+        variables = pd.concat(variables, ignore_index=True)
+
+    # ngram term frequency
+    count_vectorizer = CountVectorizer(stop_words='english', min_df=min_doc_freq,
+                                       max_df=max_doc_freq, ngram_range=(n, n))
+    count_vectorizer.fit(variables)
+    ngram_dict = count_vectorizer.vocabulary_
+    ngram_list_sorted = sorted(
+        ngram_dict.items(), key=lambda x: x[1], reverse=True)
+    freq_column_name = str(n) + 'gram_tf'
+    freq_perc_column_name = str(n) + 'gram_tf_perc'
+    ngram_tf_df = pd.DataFrame(ngram_list_sorted, columns=[
+                               'term', freq_column_name])
+    ngram_tf_df[freq_perc_column_name] = ngram_tf_df[freq_column_name].apply(
+        lambda x: x/ngram_tf_df.shape[0])
+
+    # ngram inverse document frequency
+    idf_vec = TfidfVectorizer(stop_words='english', min_df=min_doc_freq,
+                              max_df=max_doc_freq, ngram_range=(n, n))
+    x = idf_vec.fit_transform(variables)
+
+    if print_stats:
+        print('sparse matrix shape:', x.shape)
+        print('nonzero count:', x.nnz)
+        sparsity = 100.0 * x.nnz / (x.shape[0] * x.shape[1])
+        print('sparsity: %.2f%%' % sparsity)
+    # average ngram idf for each row
+    weights = np.asarray(x.mean(axis=0)).ravel().tolist()
+    idf_column_name = str(n) + 'gram_idf'
+    ngram_tf_idf_df = pd.DataFrame(
+        {'term': idf_vec.get_feature_names(), idf_column_name: weights})
+
+    # merge the two dataframes
+    ngram_tf_idf = pd.merge(
+        ngram_tf_df, ngram_tf_idf_df, on='term', how='outer')
+    return ngram_tf_idf
+
+
 # Series and numpy array functions
 
 def ordinal_to_indicator(numpy_array):
@@ -237,6 +290,28 @@ def variable_match(variable_1, variable_2):
     return (variable_1 == variable_2).astype(int)
 
 
+def stem_variable(variable):
+    """
+    Stem the text
+    :param variable: pandas.Series
+    :return: pandas.Series
+    """
+    stemmer = SnowballStemmer("english")
+    return variable.map(lambda x: ' '.join([stemmer.stem(y) for y in x.decode('utf-8').split(' ')]))
+
+
+def ngram_idf_sum(variable, ngram_tf_df, n):
+    """
+    Returns a series of floats derived from the average
+    ngram idf values for each element in the series
+    :param variable: pandas.Series
+    :param ngram_tf_df: pandas.DataFrame
+    :param n: int
+    :return: pandas.Series
+    """
+    return variable.apply(lambda x: ngram_idf_sum_on_string(x, ngram_tf_df, n))
+
+
 # Misc
 
 def exist_nan(pandas_series):
@@ -247,3 +322,24 @@ def exist_nan(pandas_series):
     """
     return pandas_series.isnull().values.any()
 
+
+def ngram_idf_sum_on_string(string, ngram_tf_df, n):
+    """
+    Average the ngram idf values of a given string;
+    meant to be used with the ngram_idf_sum function
+    :param string: str
+    :param ngram_tf_df: pandas.DataFrame
+    :param n: int
+    :return: float
+    """
+    count_vectorizer = CountVectorizer(
+        stop_words='english', ngram_range=(n, n))
+    # handle empty strings
+    try:
+        count_vectorizer.fit([string])
+        ngram_list = list(count_vectorizer.vocabulary_.keys())
+        idf_df = ngram_tf_df[ngram_tf_df['term'].isin(ngram_list)]
+        ngram_idf_linear_combo = idf_df.iloc[:, -1].sum()
+        return ngram_idf_linear_combo
+    except:
+        return 0.0
